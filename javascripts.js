3117bf1709d2b9b1182b88ba82dcdf7fa9e8b12d
Claude Odne <31524842+claude-odne01@users.noreply.github.com>
2022-06-05 16:31:34 -0400

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,54 @@
+let myheight = document.getElementById('height1');
+let myweight = document.getElementById('weight1');
+
+let flyheight = document.getElementById('height2');
+let flyweight = document.getElementById('weight2');
+
+let bmioutput = document.getElementById('BMIoutput');
+
+let temp = document.getElementById('temp1');
+
+//   ********************** Calculate BMI ******************************
+function myfun() {
+     let johnbmi = myweight.value / ( myheight.value * myheight.value );
+     
+     let lucbmi = flyweight.value / ( flyheight.value * flyheight.value );
+     
+     johnbmi = Number.parseFloat(johnbmi).toFixed(2);
+     lucbmi = Number.parseFloat(lucbmi).toFixed(2);
+     
+     bmioutput.innerHTML = `Johns BMI: ${johnbmi} bmi - Lucas' BMI: ${lucbmi} bmi - Lucas' BMI is higher than Johns BMI: That statement is ${ ( johnbmi.value > lucbmi.value )}`;
+     
+     // *********** Console Output *************************************
+     if( johnbmi > lucbmi ) {
+          console.log(`Johns BMI ( ${johnmbi} ) is higher than Lucas' BMI ( ${lucbmi} )`);
+     }
+     else {
+          console.log(`Lucas' BMI ( ${lucmbi} ) is higher than Johns BMI ( ${johnbmi} )`);
+     }
+}
+// *************************************************************************
+
+// ********************* Convert Tempterature ***********************
+function myfun1() {
+     let check = document.getElementById('fars1').checked;
+     let temp = document.getElementById('temp1');
+     let out1 = document.getElementById('Tempoutput');
+     
+     let celsi = 5 / 9 * (temp.value - 32) ;
+     let farnh = temp.value * (9 / 5) + 32  ;
+     
+     celsi = Number.parseFloat(celsi).toFixed(1);
+     farnh = Number.parseFloat(farnh).toFixed(1);
+     
+     if (check) {
+          out1.innerHTML = `Temperature in Celsius: ${celsi} degrees`;
+     }
+     else {
+          out1.innerHTML = `Temperature in Farenheit: ${farnh} degrees`;
+     }
+}
+// ***************************************************************************
+
+conBtn.addEventListener("click", myfun);
+conBtn1.addEventListener("click", myfun1);
